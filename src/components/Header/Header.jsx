68fd07e49243edd048ee21dda6f5ff3955b50072
L0la <torok.gabi@gmail.com>
2021-03-03 15:35:36 +0100

    aria label settings

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,20 +9,20 @@ import "./header.scss";
 const Header = ({sidebar, clickHandler}) => {
     
     return (
-        <header className="header">
+        <header className="header" aria-label="header">
             <img className="mobile-img" src={bgMobile} alt=""/>
             <img className="desktop-img" src={bgDesktop} alt=""/>
-            <div className="menu">
+            <div className="menu" aria-label="menu">
                 <img src={logo} alt="logo img"/>
-                <div className={sidebar ? "hamburger active" : "hamburger"}>
-                    <span className="hamburger-icon" onClick={clickHandler}><img src={hamburger} alt="hamburger icon"/></span>
+                <div className={sidebar ? "hamburger active visually-hidden-focusable" : "hamburger"}>
+                    <span className="hamburger-icon" onClick={clickHandler}><img src={hamburger} alt="hamburger icon" role="button"/></span>
                 </div>
-                <div className={sidebar ? "close active" : "close"}>
-                    <span className="close-icon" onClick={clickHandler}><img src={close} alt="menu close icon"/></span>
+                <div className={sidebar ? "close active visually-hidden-focusable" : "close"}>
+                    <span className="close-icon" onClick={clickHandler}><img src={close} alt="menu close icon" role="button"/></span>
                 </div>
             </div>
-            <div className={sidebar ? "nav-menu active" : "nav-menu"}>            
-                <ul className="nav-list">
+            <div className={sidebar ? "nav-menu active" : "nav-menu"} aria-label="nav-menu">            
+                <ul className="nav-list" aria-labelledby="nav-list">
                     <li className="border"><a href="" className="nav-item">About</a></li>
                     <li className="border"><a href="" className="nav-item">Discover</a></li>
                     <li className="border"><a href="" className="nav-item">Get Started</a></li>
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -16,103 +16,97 @@ const Project = ({select, clickSelect, sidebar}) => {
     }
     
     const clickBamboo = () => {
-        setBamboo(!bamboo);
-        
+        setBamboo(!bamboo);       
     }
 
     const clickBlackEdition = () => {
         setBlackEdition(!blackEdition);
     }
-
-    const clickBorder = () => {
-        setBorder(!border);
-    }
-    
     return (
         <>
-            <div className={select ? "project-container active" : "project-container"}>
+            <div className={select ? "project-container active" : "project-container"} aria-label="project-container">
                 <div className={sidebar ? "close active" : "close"}>
                     <h2>Back this project</h2>
-                    <span onClick={clickSelect}><img src={Close} alt="close icon"/></span>
+                    <span onClick={clickSelect}><img src={Close} alt="close icon" role="button"/></span>
                 </div>
                 <p className="subtitle">Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
-                <section className={border ? "stand-content active" : "stand-content"}>
-                    <div className="title-content">
+                <section className={border ? "stand-content active" : "stand-content"} aria-label="stand-content">
+                    <div className="title-content" aria-label="title-content">
                         <div className={border ? "radio-btn active" : "radio-btn"} onClick={clickSupport}>
                             <label>
-                                <input  className="radio" type="radio" name="support" value="no reward"/>
+                                <input className="radio" type="radio" name="support" value="no reward" role="checkbox"/>
                             </label>
                         </div>
                         <h4 className="title">Pledge with no reward</h4>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p>Choose to support us without a reward if you simply believe in our project. As a backer, 
                             you will be signed up to receive product updates via email.</p>
                     </div>
                 </section>
-                <section className={bamboo ? "stand-content active" : "stand-content"}>
-                    <div className="title-content">
+                <section className={bamboo ? "stand-content active" : "stand-content"} aria-label="stand-content">
+                    <div className="title-content" aria-label="title-content">
                         <div className={bamboo ? "radio-btn active" : "radio-btn"} onClick={clickBamboo}>
                             <label>
-                                <input className="radio" name="support" type="radio" value="bamboo stand"/>
+                                <input className="radio" name="support" type="radio" value="bamboo stand" role="checkbox"/>
                             </label>
                         </div>                       
                         <h4 className="title">Bamboo Stand</h4>
                         <p className="pledge">Pledge $25 or more</p>
                         <p className="mobile-none"><span className="number">101</span>left</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p className="description-text">You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
                             you’ll be added to a special Backer member list.</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p className="stock"><span className="stock-number">101</span> left</p>
                     </div>
-                    <div className={bamboo ? "btn-description active" : "btn-description"}>
+                    <div className={bamboo ? "btn-description active" : "btn-description"} aria-label="btn-description">
                         <p className="enter-pledge">Enter your pledge</p>
-                        <div className="btn-container">
-                            <button className="pledge-btn">$25</button>
-                            <button onClick={clickSupport} className="stand-btn">Continue</button>
+                        <div className="btn-container" aria-label="btn-container">
+                            <button className="pledge-btn" role="button">$25</button>
+                            <button onClick={clickSupport} className="stand-btn" role="button">Continue</button>
                         </div>
                     </div>
                 </section>
-                <section className={blackEdition ? "stand-content active" : "stand-content"}>
+                <section className={blackEdition ? "stand-content active" : "stand-content"} aria-label="stand-content">
                     <div className="title-content">
                         <div className={blackEdition ? "radio-btn active" : "radio-btn"} onClick={clickBlackEdition}>
                             <label>
-                                <input className="radio" type="radio" name="support" value="black edition stand"/>
+                                <input className="radio" type="radio" name="support" value="black edition stand" role="checkbox"/>
                             </label>
                         </div>                       
                         <h4 className="title">Black Edition Stand</h4>
                         <p className="pledge">Pledge $75 or more</p>
                         <p className="mobile-none"><span className="number">64</span> left</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p className="description-text">You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer
                             member list. Shipping is included.</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p className="stock"><span className="stock-number">64</span> left</p>
                     </div>
-                    <div className={blackEdition ? "btn-description active" : "btn-description"}>
+                    <div className={blackEdition ? "btn-description active" : "btn-description"} aria-label="btn-description">
                         <p className="enter-pledge">Enter your pledge</p>
                         <div className="btn-container">
-                            <button className="pledge-btn">$75</button>
-                            <button onClick={clickSupport} className="stand-btn">Continue</button>
+                            <button className="pledge-btn" role="button">$75</button>
+                            <button onClick={clickSupport} className="stand-btn" role="button">Continue</button>
                         </div>
                     </div>
                 </section>
-                <section className="stand-content mahogany">
-                    <div className="title-content">
+                <section className="stand-content mahogany" aria-label="stand-content">
+                    <div className="title-content" aria-label="title-content">
                         <h4>Mahogany Special Edition</h4>
                         <p className="pledge">Pledge $200 or more</p>
                         <p className="mobile-none"><span className="number">0</span> left</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added
                             to our Backer member list. Shipping is included.</p>
                     </div>
-                    <div className="description">
+                    <div className="description" aria-label="description">
                         <p className="stock"><span className="stock-number">0</span> left</p>
                     </div>
                 </section>
