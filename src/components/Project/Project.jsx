6e98c744ac41220e841b4f2d169ae054a7112255
L0la <torok.gabi@gmail.com>
2021-03-05 08:00:12 +0100

    button and button hover settings

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -31,7 +31,7 @@ const Project = ({select, clickSelect, sidebar}) => {
                 </div>
                 <p className="subtitle">Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
                 <section className={border ? "stand-content active" : "stand-content"}>
-                    <div className="title-content" aria-label="title-content">
+                    <div className="title-content">
                         <div className={border ? "radio-btn active" : "radio-btn"} onClick={clickSupport}>                           
                             <label>
                                 <input className="radio" type="radio" name="support" value="no reward"/>
@@ -39,27 +39,27 @@ const Project = ({select, clickSelect, sidebar}) => {
                         </div>
                         <h4 className="title">Pledge with no reward</h4>
                     </div>
-                    <div className="description" aria-label="description">
+                    <div className="description">
                         <p>Choose to support us without a reward if you simply believe in our project. As a backer, 
                             you will be signed up to receive product updates via email.</p>
                     </div>
                 </section>
                 <section className={bamboo ? "stand-content active" : "stand-content"}>
-                    <div className="title-content" aria-label="title-content">
+                    <div className="title-content">
                         <div className={bamboo ? "radio-btn active" : "radio-btn"} onClick={clickBamboo}>
                             <label>
                                 <input className="radio" name="support" type="radio" value="bamboo stand"/>                          
                             </label>
                         </div>                       
-                        <h4 className="title" aria-label="title">Bamboo Stand</h4>
+                        <h4 className="title">Bamboo Stand</h4>
                         <p className="pledge">Pledge $25 or more</p>
                         <p className="mobile-none"><span className="number">101</span>left</p>
                     </div>
-                    <div className="description" aria-label="description">
+                    <div className="description">
                         <p className="description-text">You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
                             you’ll be added to a special Backer member list.</p>
                     </div>
-                    <div className="description" aria-label="description">
+                    <div className="description">
                         <p className="stock"><span className="stock-number">101</span> left</p>
                     </div>
                     <div className={bamboo ? "btn-description active" : "btn-description"}>
@@ -91,14 +91,14 @@ const Project = ({select, clickSelect, sidebar}) => {
                     <div className={blackEdition ? "btn-description active" : "btn-description"}>
                         <p className="enter-pledge">Enter your pledge</p>
                         <div className="btn-container">
-                            <button className="pledge-btn" role="button">$75</button>
+                            <button className="pledge-btn">$75</button>
                             <button onClick={clickSupport} className="stand-btn">Continue</button>
                         </div>
                     </div>
                 </section>
                 <section className="stand-content mahogany">
                     <div className="title-content">
-                        <h4 aria-label="title">Mahogany Special Edition</h4>
+                        <h4>Mahogany Special Edition</h4>
                         <p className="pledge">Pledge $200 or more</p>
                         <p className="mobile-none"><span className="number">0</span> left</p>
                     </div>
